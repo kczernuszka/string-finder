f9486f5a882f493eebce1f924d7f9a0615a9c319
Karol Czernuszka <kczernuszka@o2.pl>
2020-07-22 17:08:41 +0200

    Show strings only with minimum characters

--- string_finder.cpp
+++ string_finder.cpp
@@ -2,6 +2,12 @@
 
 namespace string_finder {
 
+const int DEFAULT_MIN_CHARS = 3;
+
+StringFinder::StringFinder() : min_chars{DEFAULT_MIN_CHARS} {}
+
+StringFinder::StringFinder(int min_chars) : min_chars{min_chars} {}
+
 std::vector<std::string> StringFinder::find_strings(std::string file) {
   std::ifstream buffer{file, std::ios::binary};
   std::vector<std::string> stringsVector;
@@ -9,7 +15,9 @@ std::vector<std::string> StringFinder::find_strings(std::string file) {
   while(buffer.get(c)) {
     if(isprint(c)) {
       buffer.putback(c);
-      stringsVector.push_back(get_string(buffer));
+      std::string str = get_string(buffer);
+      if(!str.empty())
+        stringsVector.push_back(str);
     }
   }
   return stringsVector;
@@ -20,7 +28,11 @@ std::string StringFinder::get_string(std::ifstream& buffer) {
   char c;
   while(buffer.get(c) && isprint(c))
     str.push_back(c);
-  return str;
+  str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
+  if(str.size() >= min_chars)
+    return str;
+  else
+    return {};
 }
 
 }  // namespace string_finder
--- string_finder.h
+++ string_finder.h
@@ -1,6 +1,7 @@
 #ifndef STRINGFINDER_STRINGFINDER_H
 #define STRINGFINDER_STRINGFINDER_H
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -11,9 +12,12 @@ namespace string_finder {
 
 class StringFinder {
  public:
+  StringFinder();
+  StringFinder(int min_char);
   std::vector<std::string> find_strings(std::string file);
  
  private:
+  int min_chars;
   std::string get_string(std::ifstream& buffer);
 };
 
